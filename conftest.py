31229a25603a24b166916e73c0556f567084964a
Герыч Герыч <german_mel@mail.ru>
2021-08-02 14:12:49 +0300

    default language is en

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action ='store', default = "chrome",
                     help = "Choose browser: chrome or firefox")
-    parser.addoption("--language", action = "store", default = None,
+    parser.addoption("--language", action = "store", default = "en",
                     help = "Choose language: en, ru and etc")
 
 @pytest.fixture(scope="function")
