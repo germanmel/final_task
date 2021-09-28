ca35cc557b5b08f44e6f1aabc880b76a4a6bb34c
german_mel <german_mel@mail.ru>
2021-09-29 00:36:21 +0300

    Added class TestUserAddToBasketFromProductPage contains new tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,10 @@
+from .pages.base_page import BasePage
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage, urls, base_url, product_link
 from .pages.locators import ProductPageLocators
 from .pages.basket_page import BasketPage
 import time, pytest
+from faker import Faker
 
 
 #@pytest.mark.parametrize('link', urls)
@@ -55,4 +58,30 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     cart_page.should_be_empty_basket_message()
     cart_page.should_be_continue_shopping_link_message()
     cart_page.should_not_be_goods_in_basket()
-    
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        fake = Faker()
+        email = fake.email()
+        password = fake.password()
+        page = ProductPage(browser, base_url)
+        page.open()
+        log_page = LoginPage(browser, browser.current_url)
+        log_page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, base_url)
+        page.open()
+        page.push_add_to_cart()
+        page.solve_quiz_and_get_code()
+        page.should_be_alert_added_to_cart()
+        page.should_be_alert_cart_cost()
+        page.check_name_of_book()
+        page.check_price()
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, base_url)
+        page.open()
+        page.should_not_be_success_message()
